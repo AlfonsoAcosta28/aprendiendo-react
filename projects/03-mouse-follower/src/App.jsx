75f2da4958e4bc8ad90535e703ad9bdb03212b94
Alfonso28 <alfonsoacosta207@gmail.com>
2024-10-23 23:15:26 -0600

    Desappear cursor

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -21,6 +21,15 @@ const FollowMouse= ()=>{
       window.removeEventListener('pointermove', handleMove)
     }
   }, [enabled])
+
+
+  useEffect(()=>{
+    document.body.classList.toggle('no-cursor', enabled)
+
+    return () => {
+      document.body.classList.remove('no-cursor')
+    }
+  },[enabled])
   return (
     <>
       <div style={{
